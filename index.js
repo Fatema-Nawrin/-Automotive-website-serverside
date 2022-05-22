fdc65576d5ee0bfae3c97e74e678364db78c1fe6
Fatema-Nawrin <nawrin4000@gmail.com>
2022-05-23 01:34:24 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+// midlleware
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ykv1v.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('blackstone_automotor').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Blackstone Tire website server')
