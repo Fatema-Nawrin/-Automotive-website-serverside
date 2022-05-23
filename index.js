5739f3d0d1a516571fd8c7b0e2ebff378dae23ab
Fatema-Nawrin <nawrin4000@gmail.com>
2022-05-24 02:33:12 +0600

    get api for product by id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 // midlleware
@@ -22,7 +22,12 @@ async function run() {
             const query = {};
             const products = await productsCollection.find(query).toArray();
             res.send(products);
-
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
         })
 
     }
